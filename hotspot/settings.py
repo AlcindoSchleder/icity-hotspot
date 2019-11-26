c197b6d07bb84a0e1cceb276c9a3723a4d23d8a5
Alcindo Schleder <alcindoschleder@gmail.com>
2019-11-26 02:49:00 -0300

    ALLOWED_HOSTS split string

--- hotspot/settings.py
+++ hotspot/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', True)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', 'localhost,127.0.0.1')
+ALLOWED_HOSTS = ALLOWED_HOSTS.split(',')
 
 # Application definition
 
