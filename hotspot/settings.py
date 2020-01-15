69d9f20f56c852b23bf0e47efdd9c4917270d83b
Alcindo <alcindoschleder@gmail.com>
2020-01-14 23:22:27 -0300

    nova página de login

--- hotspot/settings.py
+++ hotspot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
